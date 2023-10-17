d49812f4876fc14536fa9ca5a0112d7b64582cea
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-10-17 18:50:17 +0530

    Session Provider Added for Authentication
    - Login functionality added

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 import Image from 'next/image'
 import React from 'react'
+import { useSession, signIn, signOut } from "next-auth/react"
 import { HiSearch, HiBell, HiChat} from "react-icons/hi";
 
 export default function Header() {
+    const { data: session } = useSession()
+
   return (
     <div className='flex gap-3 md:gap-2 items-center p-3 justify-between'>
         <Image src = '/pinterest-logo.png' alt='logo' height={50} width={50}
@@ -15,13 +20,20 @@ export default function Header() {
         <div className='bg-gray-200 hidden sm:flex rounded-full p-3 gap-3 items-center flex-grow'>
             <HiSearch className = 'text-2xl text-gray-500 '/>
             <input type="text" name="" id="" placeholder='Search' 
-            className='bg-transparent outline-none'/>
+            className='bg-transparent outline-none w-full'/>
         </div>
         <HiBell className= 'text-[40px] text-gray-500 hidden md:block hover:bg-gray-200 p-[0.5rem] rounded-full cursor-pointer'/>
         <HiChat className= 'text-[40px] text-gray-500 hidden md:block hover:bg-gray-200 p-[0.5rem] rounded-full cursor-pointer'/>
-        <Image src= '/profile-picture-image.jpg' alt='profile-picture' height={50} width={50}
-            className='rounded-full p-[0.5rem] hover:bg-gray-200 cursor-pointer'
-        />
+        {
+            session?.user? 
+            <Image src= {session?.user?.image as string} alt='profile-picture' height={50} width={50}
+                className='rounded-full p-[0.5rem] hover:bg-gray-200 cursor-pointer'
+            />
+            :
+            <button onClick={() => signIn()} className='p-[0.5rem] rounded-full px-4 font-semibold'>Login</button>
+        }
+        
+        
     </div>
   )
 }
---
+++ app/provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React, {ReactNode} from 'react'
+import { SessionProvider } from "next-auth/react"
+
+interface ProviderProps {
+    children: ReactNode;
+  }
+
+export default function Provider({children} : ProviderProps) {
+  return (
+    <SessionProvider>
+        {children}
+    </SessionProvider>
+  )
+}
