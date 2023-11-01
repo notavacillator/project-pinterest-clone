ed341bfffe64af11dc6ca948f7ab3acd5ceeda3b
Aman Singh Bhadauria <bhadauria.shivam15@gmail.com>
2023-11-01 16:51:51 +0530

    refactor, beautify

--- app/page.tsx
+++ app/page.tsx
@@ -6,38 +6,25 @@ import { collection, getDocs, getFirestore, query, where } from "firebase/firest
 import app from "./shared/firebase.config";
 import PinsList from "./components/pinsList";
 
-
 export default function Home() {
   const [listOfPin, setListOfPin] = useState<PinterestPost[]>([])
   const db = getFirestore(app);
 
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    if(loading)
     getAllPins();
-  }, [loading]);
+  }, []);
   
   const getAllPins = async () => {
-    setLoading(true);
     setListOfPin([]);
     const q = query(collection(db, 'pinterest-post'));
     const querySnapshot = await getDocs(q);
-    console.log(querySnapshot);
     
     querySnapshot.forEach((doc) => {
       setListOfPin((listOfPins) => [...listOfPins, doc.data()] as PinterestPost[]);
-    
     })
-    
-    setLoading(false); // Data fetching is complete
-    
   } 
   
-  
-  
   return (
-    
   <>
     <main className="p-3">
       <PinsList listOfPins = {listOfPin}/>
